a9dc470077111ebd17b8949cfd206fcd4e3dfe99
Okabe Masashi (岡部 将司) <24020@ncsad.local>
2025-02-18 16:04:40 +0900

    added 認証追加

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "test2402054df6f18": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
