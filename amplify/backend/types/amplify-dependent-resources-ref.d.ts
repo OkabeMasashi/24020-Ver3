228c584a3c52e11b89a7a33b6c650d1e1ec1afa0
Okabe Masashi (岡部 将司) <24020@ncsad.local>
2025-02-19 11:19:28 +0900

    ストレージ 画像の保存

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "test24020": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "test2402054df6f18": {
       "AppClientID": "string",
@@ -8,6 +15,12 @@ export type AmplifyDependentResourcesAttributes = {
       "UserPoolArn": "string",
       "UserPoolId": "string",
       "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "Storage24020": {
+      "BucketName": "string",
+      "Region": "string"
     }
   }
 }
